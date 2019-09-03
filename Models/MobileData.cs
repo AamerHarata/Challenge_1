a4467449e522346bc961c13cd6127633836988f7
Aamer Harata <Aamer.harata@hotmail.com>
2019-09-04 00:15:46 +0200

    id

--- Models/MobileData.cs
+++ Models/MobileData.cs
@@ -1,8 +1,10 @@
+using System;
+
 namespace TestGoogle.Models
 {
     public class MobileData
     {
-        public int Id { get; set; }
+        public int Id { get; set; } = new Random().Next(0000000, 9999999); 
         public string xAco { get; set; }
         public string yAco { get; set; }
         public string zAco { get; set; }
